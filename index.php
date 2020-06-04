88e65308323ca8d188f2d8b369b8c85d7bcbf58d
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2020-06-04 10:51:37 -0300

    create generic class 1

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
+
 <?php
-
-
-	echo "teste";
-
-
+    
+    #importando classes PHP
+    require_once 'models/genericPHPClass1.php'; 
+    
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arquitetura PHP</title>
+</head>
+<body>
+
+    <?php
+        echo "teste";
+    ?>
+
+</body>
+</html>
---
+++ models/genericPHPClass1.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Classe: genericPHPClass1.php
+ * Autor: Weuller Marcos
+ * Criada em: xx/xx/20xx
+ */
+
+class genericPHPClass1 {
+    
+    #exemplos de atributos
+    var $attr1;
+    var $attr2;
+	
+    #metodo genérico 1 
+    function genericMethod1()
+    {
+        # code...
+    }
+    
+}
