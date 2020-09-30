793aff6dea0fe9e36c5034851783224593cf2523
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2020-09-29 21:14:49 -0300

    atualizando mesages

--- config/messages.php
+++ config/messages.php
@@ -8,36 +8,120 @@
  */
 
 
-//Mensagens do site:
-define('CAMPOS_PREENCHIMENTO_OBRIGATORIO' , 'Os seguintes campos são de preenchimento obrigatório: ');
-define('CADASTRO_COM_SUCESSO'             , 'Cadastro realizado com sucesso!');
-define('CADASTRO_COM_ERRO'                , 'Erro ao realizar cadastro!');
-define('USUARIO_SENHA_INVALIDOS'          , 'Usuário ou senha inválido, favor verifique os dados digitados!');
-define('LOGIN_PARA_VER_CONTEUDO'          , 'Favor realizar login para visualizar este conteúdo!');
-define('ERRO_LOGIN'                       , 'Erro ao realizar login!');
-define('DESCONECTAR'                      , 'Deseja realmente desconectar?');
-define('SELECIONE_CIDADE'                 , 'Selecione a Cidade');
-define('MENSAGEM_DINHEIRO'                , 'Dinheiro');
-define('MENSAGEM_DEBITO'                  , 'Débito');
-define('MENSAGEM_CREDITO'                 , 'Crédito');
-define('MENSAGEM_TODOS'                   , 'Todos');
-define('MENSAGEM_FILTROS'                 , 'Filtros: ');
-define('FILTRAR_STATUS_PEDIDO'            , 'Filtrar por Status do Pedido');
-define('MENSAGEM_ESCOLHA_REVENDEDOR'      , 'Escolha o Revendedor: ');
-define('MENSAGEM_FORMA_PAGAMENTO'         , 'Forma de Pagamento');
+//* Mensagens do site */
+
+
+//A:
+
+define('ALTERAR_SENHA'                    , 'Alterar senha');
+define('ATUALIZAR_DADOS'                  , 'Atualizar dados');
+
+//B:
+
 define('BEM_VINDO'                        , 'Bem-Vindo!');
+
+//C:
+
+define('CADASTRO_COM_ERRO'                , 'Erro ao realizar cadastro!');
+define('CADASTRO_COM_SUCESSO'             , 'Cadastro realizado com sucesso!');
+define('CAMPOS_PREENCHIMENTO_OBRIGATORIO' , 'Os seguintes campos são de preenchimento obrigatório: ');
+define('CANCELAR'                         , 'Cancelar');
+define('CONFIRMAR_SENHA_NAO_CONFERE'      , 'Os campos de Nova Senha e Confirmação de senha não conferem!');
+define('CREDITO'                          , 'Crédito');
+
+//D:
+
 define('DASHBOARD'                        , 'Dashboard');
-define('SAIR'                             , 'Sair');
+define('DEBITO'                           , 'Débito');
+define('DESEJA_DESCONECTAR'               , 'Deseja realmente desconectar?');
+define('DINHEIRO'                         , 'Dinheiro');
+
+//E:
+
+define('EMAIL'                            , 'E-mail');
+define('ERRO_ALTERAR_SENHA'               , 'Erro ao alterar senha!');
+define('ERRO_LOGIN'                       , 'Erro ao realizar login!');
+define('ESCOLHA_REVENDEDOR'               , 'Escolha o Revendedor: ');
+define('ESQUECI_MINHA_SENHA'              , 'Esqueci minha senha');
+
+//F:
+
+define('FILTRAR_STATUS_PEDIDO'            , 'Filtrar por Status do Pedido');
+define('FILTROS'                          , 'Filtros: ');
+define('FORMA_PAGAMENTO'                  , 'Forma de Pagamento');
+
+//G:
+
+
+//H:
+
+define('HABILITAR_ALTERACAO'              , 'Habilitar alteração');
+
+//I:
+
+
+//J:
+
+
+//K:
+
+
+//L:
+
+define('LEMBRE_ME'                        , 'Lembre-me');
+define('LOGIN'                            , 'Login');
+define('LOGIN_PARA_VER_CONTEUDO'          , 'Favor realizar login para visualizar este conteúdo!');
+
+//M:
+
 define('MEU_PERFIL'                       , 'Meu Perfil');
 
+//N:
 
-/*Senha*/
-define('CONFIRMAR_SENHA_NAO_CONFERE'      , 'Os campos de Nova Senha e Confirmação de senha não conferem!');
+
+//O:
+
+
+//P:
+
+
+//Q:
+
+
+//R:
+
+
+//S:
+
+define('SAIR'                             , 'Sair');
+define('SELECIONE_CIDADE'                 , 'Selecione a Cidade');
+define('SENHA'                            , 'Senha');
 define('SENHA_ANTIGA_NAO_CONFERE'         , 'A senha antiga digitada não confere!');
 define('SENHAS_NAO_CONFEREM'              , 'As senhas digitadas não conferem!');
 define('SENHAS_PREENCHIMENTO_OBRIGATORIO' , 'Os campos de senha são de preenchimento obrigatório!');
 define('SUCESSO_ALTERAR_SENHA'            , 'Senha alterada com sucesso!');
-define('ERRO_ALTERAR_SENHA'               , 'Erro ao alterar senha!');
+
+//T:
+
+define('TODOS'                            , 'Todos');
+
+//U:
+
+define('USUARIO_SENHA_INVALIDOS'          , 'Usuário ou senha inválido, favor verifique os dados digitados!');
+
+//V:
+
+
+//W:
+
+
+//X:
+
+
+//Y:
+
+
+//Z:
 
 
 ?>
